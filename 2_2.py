104df72baf1f1d0f49e5124941ea76b631767abb
Helena Wang <helena@altamotors.com>
2017-12-13 12:38:01 -0800

    Commented and completed 2_2

--- 2_2.py
+++ 2_2.py
@@ -2,19 +2,20 @@ import numpy as np
 
 inputArray = np.loadtxt('2_2_input.txt')
 
-total=0
+total=0 #running sum of quotients
 currentRow=0
 
-
-for i in range(0,inputArray.shape[1]-1):
-    checkingElement=inputArray[currentRow, i] #pulls elements of row in order
-    for j in range(0, inputArray.shape[1]-1):
-        compareElement=inputArray[currentRow, j]
-        #print (checkingElement, compareElement)
-        if (checkingElement%compareElement)==0 or (compareElement%checkingElement)== 0: #checks each element against other elements in row
-            if j!=i:
-                total+= max(checkingElement,compareElement)/min(checkingElement,compareElement)
-                currentRow+=1
-                print (currentRow)
-                print (total)
+while currentRow < inputArray.shape[0]: #for each row in the array
+    for i in range(0,inputArray.shape[1]):
+        sortedRow= sorted(inputArray[currentRow], reverse=True)
+        checkingElement=sortedRow[i] #pulls elements of row in decreasing order
+        for j in range(1, inputArray.shape[1]):
+            compareElement=sortedRow[j]
+            if (checkingElement%compareElement)==0: #checks each element against other elements in row
+                if j!=i: #not dividing something by itself
+                    total+= checkingElement/compareElement
+                    currentRow+=1
+                    print (currentRow)
+                    print (total)
+                
 
